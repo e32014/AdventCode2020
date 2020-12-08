cddaadf98ac1e771bc232c20ef8b6dc367dee47e
Evan Klei <e32014@gmail.com>
2020-12-08 00:15:51 -0500

    Day 8, bit of a brute force, but some cute shortcircuting on some of the conditions

---
+++ HandheldGame/HandheldGame.py
@@ -0,0 +1,36 @@
+import copy
+
+file = open("input.txt")
+
+acc = 0
+visited = set()
+pc = 0
+
+program = file.readlines()
+
+for i in range(0, len(program)):
+    progDupe = copy.deepcopy(program)
+    repKey, repVal = program[i].strip().split(" ")[0:2]
+    if repKey == 'nop':
+        progDupe[i] = 'jmp '+repVal
+    elif repKey == 'jmp':
+        progDupe[i] = 'nop '+repVal
+    else:
+        continue
+    while pc not in visited and pc < len(program):
+        line = progDupe[pc]
+        visited.add(pc)
+        key, val = line.strip().split(" ")[0:2]
+        if key == 'nop':
+            pc += 1
+        elif key == 'acc':
+            acc += int(val)
+            pc += 1
+        elif key == 'jmp':
+            pc += int(val)
+    if pc >= len(program):
+        print(acc)
+        break
+    pc = 0
+    acc = 0
+    visited = set()
